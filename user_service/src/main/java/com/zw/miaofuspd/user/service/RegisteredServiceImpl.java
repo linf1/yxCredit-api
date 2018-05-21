7a5638a6d4f0127d5323b02ec6ed6f63465292f0
chenqingyu <409284123@qq.com>
2018-05-21 17:22:37 +0800

    接口提交

--- user_service/src/main/java/com/zw/miaofuspd/user/service/RegisteredServiceImpl.java
+++ user_service/src/main/java/com/zw/miaofuspd/user/service/RegisteredServiceImpl.java
@@ -147,7 +147,7 @@ public class RegisteredServiceImpl extends AbsServiceBase implements IRegistered
         String tel = (String) map.get("tel");
         String registrationId = (String) map.get("registrationId");
         //将时间格式化
-        String creatTime =map.get("createTime") == null ? (String)map.get("createTime") : DateUtils.getDateString(new Date()) ;
+        String ceateTime =map.get("createTime") == null ? DateUtils.getDateString(new Date()) : (String)map.get("createTime");
         String onlyCodeSpd = map.get("onlyCode") == null ? "" : map.get("onlyCode").toString();
         String blackBox = "";
         String password = "";
@@ -155,7 +155,7 @@ public class RegisteredServiceImpl extends AbsServiceBase implements IRegistered
         String type = map.get("type") == null ? "" : map.get("type").toString();
         //插入app_user表
         StringBuffer insql = new StringBuffer("INSERT INTO app_user (id,tel,black_box,type,passwd,state,creat_time,alter_time,registration_id,only_code_spd) VALUES('");
-        insql.append(id + "','" + tel + "','" + blackBox + "','" + type + "','" + password + "','" + 1 + "','" + creatTime + "','" + creatTime + "','"+registrationId+"','"+onlyCodeSpd+"')");
+        insql.append(id + "','" + tel + "','" + blackBox + "','" + type + "','" + password + "','" + 1 + "','" + ceateTime + "','" + ceateTime + "','"+registrationId+"','"+onlyCodeSpd+"')");
         //插入mag_customer_person表
         sunbmpDaoSupport.exeSql(insql.toString());
         return id;
