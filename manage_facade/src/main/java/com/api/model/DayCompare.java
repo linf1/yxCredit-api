999cfff6d78c6e61cd6e678af9c09bf1fc7d9fda
陈清玉 <409284123@qq.com>
2018-06-01 15:31:13 +0800

    日期对比

--- manage_facade/src/main/java/com/api/model/DayCompare.java
+++ manage_facade/src/main/java/com/api/model/DayCompare.java
@@ -1,5 +1,7 @@
 package com.api.model;
 
+import com.base.util.DateUtils;
+
 import java.util.Calendar;
 import java.util.Date;
 
@@ -8,64 +10,46 @@ import java.util.Date;
  * @author 陈清玉
  */
 public class DayCompare {
-    private int year;
-    private int month;
-    private int day;
+    private Long year;
+    private Long month;
+    private Long day;
+    private Long hours;
+    private Long minutes;
 
     public DayCompare(){
     }
 
-    public DayCompare(int year, int month, int day) {
+    public DayCompare(Long year, Long month, Long day, Long hours, Long minutes) {
         this.year = year;
         this.month = month;
         this.day = day;
+        this.hours = hours;
+        this.minutes = minutes;
     }
 
-    public int getYear() {
-        return year;
-    }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
 
     /**
      * 计算2个日期之间相差的  相差多少年月日
-     * 比如：2011-02-02 到  2017-03-02 相差 6年，1个月，0天
+     * 比如：2017-02-02 到  2011-03-02 相差 6年，1个月，0天
      * @param fromDate
      * @param toDate
      * @return
      */
     public static DayCompare dayComparePrecise(Date fromDate, Date toDate){
-        Calendar from  =  Calendar.getInstance();
-        from.setTime(fromDate);
-        Calendar  to  =  Calendar.getInstance();
-        to.setTime(toDate);
-        int fromYear = from.get(Calendar.YEAR);
-        int fromMonth = from.get(Calendar.MONTH);
-        int fromDay = from.get(Calendar.DAY_OF_MONTH);
-        int toYear = to.get(Calendar.YEAR);
-        int toMonth = to.get(Calendar.MONTH);
-        int toDay = to.get(Calendar.DAY_OF_MONTH);
-        int year = toYear  -  fromYear;
-        int month = toMonth  - fromMonth;
-        int day = toDay  - fromDay;
-        return new DayCompare(year,month,day);
+        //这样得到的差值是微秒级别
+        long diff = fromDate.getTime() - toDate.getTime();
+        long days = diff / (1000 * 60 * 60 * 24);
+        long hours = (diff-days*(1000 * 60 * 60 * 24))/(1000* 60 * 60);
+        long minutes = (diff-days*(1000 * 60 * 60 * 24)-hours*(1000* 60 * 60))/(1000* 60);
+        System.out.println(""+days+"天"+hours+"小时"+minutes+"分");
+        return new DayCompare(days / 365 ,days / 30 ,days,hours,minutes);
+    }
+
+    public static void main(String[] args) {
+        Date fromDate = DateUtils.strConvertToDate("2018-05-02",DateUtils.STYLE_2);
+        Date toDate = DateUtils.strConvertToDate("2019-05-03",DateUtils.STYLE_2);
+        DayCompare dayCompare = dayComparePrecise(toDate, fromDate);
+        System.out.println(dayCompare.year+"年"+dayCompare.month+"月"+dayCompare.day+"天"+dayCompare.hours+"小时"+dayCompare.minutes+"分");
     }
 }
