e77e90e0ced06ead8e6ed356dea494064022ae82
Van <515972834@qq.com>
2018-07-04 20:05:06 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 ulimit -s 256
 export JAVA_HOME=/home/jdk1.8.0/jdk1.8.0_111
 APP_HOME=${PWD}
-programdir="." 
+programdir="."
 
-num=$# 
+num=$#
 temp=$CLASSPATH:$APP_HOME/lib:./
-#setting libs path 
-libs=lib/* 
-append(){ 
-    temp=$temp":"$1 
-} 
+#setting libs path
+libs=lib/*
+append(){
+    temp=$temp":"$1
+}
 
 basepath=${PWD}/*
 for file in $basepath; do
@@ -20,14 +20,14 @@ for file in $basepath; do
     fi
 done
 
-for file in $libs;    do 
-    append $file 
-done 
+for file in $libs;    do
+    append $file
+done
 export CLASSPATH=$temp
 #export LC_ALL="en_US.UTF-8"
 #export CLASSPATH=$CLASSPATH:$APP_HOME/lib
 #echo $CLASSPATH
 echo "Running [Trading] Service ..."
 JAVA_OPTS="-server -Xms256m -Xmx256m -Xss256K -XX:-UseGCOverheadLimit"
-nohup $JAVA_HOME/bin/java $JAVA_OPTS -classpath $CLASSPATH com.zw.service.boot.ServiceBoot > nohup.out 2>&1 &
+nohup java $JAVA_OPTS -classpath $CLASSPATH com.zw.service.boot.ServiceBoot > nohup.out 2>&1 &
 tail -f nohup.out
