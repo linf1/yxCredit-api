64da083145b0406d8afca44757f17913fce4f3b9
陈清玉 <409284123@qq.com>
2018-07-09 16:58:58 +0800

    放款接口提交

---
+++ manage_service/src/main/java/com/zw/miaofuspd/api/poxy/ApiProxy.java
@@ -0,0 +1,54 @@
+package com.zw.miaofuspd.api.poxy;
+
+import com.api.model.common.BaseSettings;
+import com.base.util.StringUtils;
+import com.zw.api.HttpUtil;
+import org.apache.http.Header;
+
+import java.util.List;
+
+/**
+ * api接口调用代理
+ * @author 陈淸玉
+ */
+public class ApiProxy {
+    private final String GET = "get";
+    private final String POST = "post";
+
+    private BaseSettings settings;
+
+    public ApiProxy(BaseSettings settings){
+        this.settings = settings;
+    }
+
+    /**
+     * API调用方法
+     * @param param 请求参数
+     * @param headerList 请求头
+     * @return 返回信息
+     */
+    public String  apiInvoke(String param,List<Header> headerList){
+        if(StringUtils.isEmpty(settings.getRequestUrl()) || StringUtils.isEmpty(settings.getRequestType())){return "";}
+        if(POST.equalsIgnoreCase(settings.getRequestType())){
+            if (isHttps(settings.getRequestUrl())) {
+                return HttpUtil.postSSL(settings.getRequestUrl(),param,headerList);
+            }else{
+                return HttpUtil.post(settings.getRequestUrl(),param,headerList);
+            }
+        }else if(GET.equalsIgnoreCase(settings.getRequestType())){
+           return HttpUtil.getSSL(settings.getRequestUrl(),headerList);
+        }
+        return "";
+    }
+
+    /**
+    * 检测是否https
+    * @param url 请求地址
+    */
+     private  boolean isHttps(String url) {
+         return url.startsWith("https");
+     }
+
+
+
+}
