836cb6d524118adfdd6abe43c0d52cd5a8428021
陈清玉 <409284123@qq.com>
2018-07-09 16:08:48 +0800

    放款信息接口

--- manage_facade/src/main/java/com/api/service/ds/AbsDSBaseServer.java
+++ manage_facade/src/main/java/com/api/service/ds/AbsDSBaseServer.java
@@ -4,18 +4,18 @@ package com.api.service.ds;
  * 数据同步基础服务
  * @author 陈清玉 create by 2018-07-09
  */
-public  interface IDSBaseServer {
+public  abstract  class AbsDSBaseServer {
     /**
      * 浙商 code
      */
-     String ZS_CODE = "316";
+     final String ZS_CODE = "316";
 
     /**
      * 根据开户行银行code判断是否是他行（0：浙商；1：非浙商）
      * @param bankCode 银行code
      * @return （0：浙商；1：非浙商）
      */
-    static String getOtherFlag(String bankCode){
+     protected String getOtherFlag(String bankCode){
         return ZS_CODE.equals(bankCode) ? "0" : "1";
     }
 }
