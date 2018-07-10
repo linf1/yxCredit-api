57090c157028853124f21b7d2b27354ace21d545
陈清玉 <409284123@qq.com>
2018-07-10 09:48:39 +0800

    省市支行 不为必填

--- manage_service/src/main/java/com/zw/miaofuspd/api/ds/DSMoneyServerImpl.java
+++ manage_service/src/main/java/com/zw/miaofuspd/api/ds/DSMoneyServerImpl.java
@@ -5,20 +5,18 @@ import com.api.model.common.BYXRequest;
 import com.api.model.common.BYXResponse;
 import com.api.model.ds.DSMoneyRequest;
 import com.api.model.ds.DSMoneySettings;
+import com.api.service.ds.AbsDSBaseServer;
 import com.api.service.ds.IDSMoneyServer;
 import com.zw.api.HttpClientUtil;
-import com.zw.miaofuspd.facade.personal.service.AppBasicInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 /**
  * 借款人及放款账户数据同步服务实现
  * @author 陈清玉
  */
 @Service(IDSMoneyServer.BEAN_KEY)
-public class DSMoneyServerImpl implements IDSMoneyServer {
+public class DSMoneyServerImpl extends AbsDSBaseServer implements IDSMoneyServer {
 
     @Autowired
     private DSMoneySettings dsMoneySettings;
@@ -30,6 +28,8 @@ public class DSMoneyServerImpl implements IDSMoneyServer {
     @Override
     public BYXResponse saveBorrowerAndAccountCard(DSMoneyRequest request ) throws Exception {
         if (request != null){
+            //设置浙商
+            request.setOtherFlag(getOtherFlag(request.getBankCode()));
             final String result = HttpClientUtil.post(dsMoneySettings.getRequestUrl(), BYXRequest.getBYXRequest(request, byxSettings), byxSettings.getHeadRequest());
             return BYXResponse.getBYXResponse(result, byxSettings);
         }
