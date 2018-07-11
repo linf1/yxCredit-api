496aaeafd49fb98e6e000f1e3a0e1357dcce4235
han <812759359@qq.com>
2018-07-11 15:14:03 +0800

    修改去申请的逻辑

--- order_service/src/main/java/com/zw/miaofuspd/personal/service/AppBasicInfoServiceImpl.java
+++ order_service/src/main/java/com/zw/miaofuspd/personal/service/AppBasicInfoServiceImpl.java
@@ -978,9 +978,9 @@ public class AppBasicInfoServiceImpl extends AbsServiceBase implements AppBasicI
         String id = GeneratePrimaryKeyUtils.getUUIDKey();
         try {
                 //新增银行卡信息
-                String sql2 = "insert into sys_bank_card (id,bank_name,bank_number,bank_subbranch_id,bank_subbranch,bank_type,is_authcard,card_number,cust_id,cust_name,prov_id,prov_name,city_id,city_name,create_time,update_time) values ('"+GeneratePrimaryKeyUtils.getUUIDKey()+"','"+map.get("bank_name")+"'," +
+                String sql2 = "insert into sys_bank_card (id,bank_name,bank_number,bank_subbranch_id,bank_subbranch,bank_type,is_authcard,card_number,cust_id,cust_name,prov_id,prov_name,city_id,city_name,create_time,update_time,tel,card) values ('"+GeneratePrimaryKeyUtils.getUUIDKey()+"','"+map.get("bank_name")+"'," +
                         "'"+map.get("bank_number")+"','"+map.get("bank_subbranch_id")+"','"+map.get("bank_subbranch")+"','0','1','"+map.get("card_number")+"','"+id+"','"+map.get("cust_name")+"'," +
-                        "'"+map.get("prov_id")+"','"+map.get("prov_name")+"','"+map.get("city_id")+"','"+map.get("city_name")+"','"+DateUtils.getNowDate()+"','"+DateUtils.getNowDate()+"')";
+                        "'"+map.get("prov_id")+"','"+map.get("prov_name")+"','"+map.get("city_id")+"','"+map.get("city_name")+"','"+DateUtils.getNowDate()+"','"+DateUtils.getNowDate()+"','"+map.get("tel")+"','"+map.get("card")+"')";
                 sunbmpDaoSupport.exeSql(sql2);
 
             String card = map.get("card").toString();
