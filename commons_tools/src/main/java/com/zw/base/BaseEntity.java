90dcadddf491024ecfb3c46944f4c90c3b708188
陈清玉 <409284123@qq.com>
2018-07-20 17:51:36 +0800

    基础baseEntity

---
+++ commons_tools/src/main/java/com/zw/base/BaseEntity.java
@@ -0,0 +1,78 @@
+package com.zw.base;
+
+import com.constants.SysConstant;
+
+import java.util.Date;
+
+/**
+ * 基础实体默认所有的pojo都继承
+ * @author  陈清玉 create by 2018-07-06
+ * @since V1.0.0
+ */
+public class BaseEntity {
+
+    /**
+     * 创建人id
+     */
+    private String createUserId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改人id
+     */
+    private String updateUserId;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    private Integer isDeleted = SysConstant.INVALID ;
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(String updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
